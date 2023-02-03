eaf2578b717d755d74accc55fa1f0737e410baf2
Bartosz Brzozowski <bartosz.brzozowski23@gmail.com>
2023-02-03 16:27:47 +0100

    remove UT with old style

--- tests/yt_dlp_mock.py
+++
@@ -1,4 +0,0 @@
-from unittest import mock
-
-YoutubeDL = mock.Mock()
-YoutubeDL().extract_info.return_value = {"title":"test_title", "artist":"test_artist", "album":"test_album", "ext":"mp4"}
--- tests/yt_dlp_test.py
+++
@@ -1,7 +0,0 @@
-
-class YoutubeDL:
-    def __init__(self, ydl_opts):
-        pass
-
-    def extract_info(self, url):
-        return {"title":"test_title", "artist":"test_artist", "album":"test_album", "ext":"mp4"}
