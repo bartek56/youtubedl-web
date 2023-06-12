c4994f4bf06f01cbfe1049a74895d8e114f0a4cc
Bartosz Brzozowski <bartosz.brzozowski23@gmail.com>
2023-06-12 22:18:38 +0200

    favicon

--- youtubedl.py
+++ youtubedl.py
@@ -76,11 +76,6 @@ youtubeConfig.initialize(CONFIG_FILE)
 
 readyToDownload = {}
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
-
 @app.route('/')
 @app.route('/index.html')
 def index():
