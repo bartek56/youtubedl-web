b7b131d357d8a211eda77bab02491617ba879bb5
Bartosz Brzozowski <bartosz.brzozowski23@gmail.com>
2024-02-04 18:05:10 +0100

    UT more flexible - move to separate files

---
+++ tests/MailApi_tests.py
@@ -0,0 +1,64 @@
+from typing import List
+import youtubedl
+import unittest
+import unittest.mock as mock
+from Common.MailManager import Mail
+
+class FlaskClientMailTestCase(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(FlaskClientMailTestCase, self).__init__(*args, **kwargs)
+        self.checked="checked"
+        self.unchecked="unchecked"
+        self.empty=""
+
+    def setUp(self):
+        youtubedl.app.config['TESTING'] = True
+        self.app = youtubedl.app.test_client()
+        self.mailManager = youtubedl.mailManager
+
+    def test_home_page(self):
+        rv = self.app.get('/index.html')
+        assert rv.status_code == 200
+        assert b'<title>Media Server</title>' in rv.data
+
+    def test_wrong_page(self):
+        rv = self.app.get('/zzzz')
+        assert rv.status_code == 404
+        assert b'404 Not Found' in rv.data
+
+    @mock.patch.object(Mail, 'initialize', return_value=False)
+    def test_contact_mailNotInitialized(self, mock_mail):
+        rv = self.app.get('/contact.html')
+        assert rv.status_code == 200
+        assert b'<title>Media Server</title>' in rv.data
+        assert b'Mail is not support' in rv.data
+
+    @mock.patch.object(Mail, 'initialize', return_value=True)
+    def test_contact_mailInitialized(self, mock_initialize_mail):
+        rv = self.app.get('/contact.html')
+        assert rv.status_code == 200
+        mock_initialize_mail.assert_called_once()
+        assert b'<title>Media Server</title>' in rv.data
+        assert b'enter text here' in rv.data
+
+    def mail(self, senderMail, textMail):
+        return self.app.post('/mail', data=dict(
+        sender=senderMail,
+        message=textMail
+    ), follow_redirects=True)
+
+    @mock.patch.object(Mail, 'sendMail')
+    def test_correct_mail(self, mock_sendMail):
+        #rv = self.mail('test@wp.pl', 'mail text')
+        #mock_sendMail.assert_called_with(self.mailManager, "bartosz.brzozowski23@gmail.com", "MediaServer", "You received message from test@wp.pl: mail text")
+        rv = self.app.post('/mail', data=dict(
+        sender="test@gmail.com",
+        message="text"))
+        self.assertEqual(rv.status_code, 200)
+        assert b'Successfull send mail' in rv.data
+
+    @mock.patch.object(Mail, 'sendMail', autospec=True)
+    def test_wrong_mail(self, mock_Gmail):
+        rv = self.mail('jkk', '')
+        assert b'You have to fill in the fields' in rv.data
